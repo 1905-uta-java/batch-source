18a6b0f76dc54b68db42c3cc0801365bde61622c
wolfe Magnus <wolfemagnus@nevada.unr.edu>
2019-05-30 10:37:06 -0500

    Altered filesystem to include complete project structure.

---
+++ Wk1-Revature/src/com/revature/wk1types/DefaultDriver.java
@@ -0,0 +1,13 @@
+package com.revature.wk1types;
+
+public class DefaultDriver
+{
+
+		static boolean bool;
+		
+		public static void main(String args[])
+		{
+			System.out.println(bool);
+		}
+		
+}
