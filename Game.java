faaa57bb8ca08e5e3231cbcaf499268cba99be16
Mantu Nguyen <mantunguyen6989@gmail.com>
2019-05-30 19:31:44 -0500

    add guessing Game.java. Finished

--- Game.java
+++ Game.java
@@ -1,4 +1,3 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Game {
@@ -6,59 +5,47 @@ public class Game {
 	public static void main(String[] args) {
 		play();
 	}
-	
-	static void play() {
+
+	public static void play() {
 		int randomNum = getRandomNumber();
 		int lowerBound = 0;
-		int higherBound = 100;
+		int upperBound = 100;
 		int attemp = 0;
 		int input = 0;
-		boolean isNum = false;
-		
 		Scanner sc = new Scanner(System.in);
 
-		while(true) {
-			
-			System.out.print("Enter the number between " + lowerBound + " and " + higherBound + ": ");
+		while (true) {
+			System.out.print("Enter the number between " + lowerBound + " and " + upperBound + ": ");
+			while (!sc.hasNextInt()) {
+				if (!sc.hasNextInt()) {
+					System.out.print("Please input a number only: ");
+					sc.next();
+				}
+			}
 			input = sc.nextInt();
-			
-			attemp++;
-			if(input == randomNum) {
+			if (input < lowerBound || input > upperBound) {
+				System.out.println(
+						"Please input a number between relevant range (" + lowerBound + " - " + upperBound + ")");
+			} else if ((randomNum - input) > 0) {
+				lowerBound = input;
+				System.out.println("Too low");
+				attemp++;
+			} else if ((randomNum - input) < 0) {
+				upperBound = input;
+				System.out.println("Too high");
+				attemp++;
+			} else if (input == randomNum) {
+				attemp++;
 				System.out.println("You guessed right, after " + attemp + " attemps, the number is " + randomNum);
 				break;
-			}
-			else {
-				if((randomNum - input) > 0) {
-					lowerBound = input;
-					System.out.println("Too low");
-				}
-				else {
-					higherBound = input;
-					System.out.println("Too high");
-					
-				}
 			}
 		}
 		sc.close();
 	}
-	
+
 	public static int getRandomNumber() {
 		double randomDouble = Math.random();
 		randomDouble = randomDouble * 100 + 1;
 		return (int) randomDouble;
-		// System.out.println(randomInt);
 	}
-	
-	public static boolean isInt(String input) { 
-	    try { 
-	        Integer.parseInt(input);
-	        return true;
-	    }
-	    catch(Exception e ) { 
-	        return false; 
-	    }
-	} 
-	
 }
-
-
