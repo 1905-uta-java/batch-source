8abd7b5cd39ba7d4f4d24fcacfe9e00670ae4ca3
carolynrehm <carolynhrehm@gmail.com>
2019-05-31 21:35:56 -0500

    the game

---
+++ Week1-CoreJava/TheGame/src/main/java/com/revature/Question.java
@@ -0,0 +1,26 @@
+package com.revature;
+
+public class Question {
+	
+	private String content;
+	
+	public Question() {
+		super();
+	}
+	
+	public Question(String content) {
+		super();
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
