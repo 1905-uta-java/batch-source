e37dc62dac00996f5335a76ca80f2e29607da666
Jason Noone <jsnnn497@gmail.com>
2019-06-12 11:24:16 -0500

    Added some more unit tests

--- Project_0/src/test/java/com/revature/testapp/TestService.java
+++ Project_0/src/test/java/com/revature/testapp/TestService.java
@@ -9,17 +9,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import com.revature.accountdao.AccountDaoImp;
 import com.revature.accountdao.UserDaoImp;
 import com.revature.accountmodel.User;
 import com.revature.accountutil.AccountMenuUtil;
 import com.revature.accountutil.UserMenuUtil;
+import com.revature.accountutil.ValidateUtil;
 
 public class TestService {
 	private static final UserDaoImp uDao = new UserDaoImp();
-	private static final AccountDaoImp aDao = new AccountDaoImp();
 	private static final UserMenuUtil uMenu = new UserMenuUtil();
 	private static final AccountMenuUtil aMenu = new AccountMenuUtil();
+	private static final ValidateUtil validate = new ValidateUtil();
 	
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
@@ -56,5 +56,49 @@ public class TestService {
 	@Test
 	public void cannotOverdraw() {
 		assertFalse(aMenu.withdrawFunds(u, 5555, 10000.00));
+	}
+	
+	@Test
+	public void validPass() {
+		assertTrue(validate.isPassword(u.getpWord()));
+		assertTrue(validate.correctPass(u.getpWord(), u.getuName()));
+	}
+	
+	@Test
+	public void validUser() {
+		assertTrue(validate.isUsername(u.getuName()));
+	}
+	
+	@Test
+	public void validEmail() {
+		assertTrue(validate.isEmail("TestEmail@gmail.com"));
+	}
+	
+	@Test
+	public void isNotPass() {
+		assertFalse(validate.isPassword("no"));
+		assertFalse(validate.correctPass("NotPass", u.getuName()));
+	}
+	
+	@Test
+	public void isNotUser() {
+		assertFalse(validate.isUsername("NotAUser"));
+	}
+	
+	@Test
+	public void isNotEmail() {
+		assertFalse(validate.isEmail("notanemail"));
+		assertFalse(validate.isEmail("not@email"));
+		assertFalse(validate.isEmail("not.email"));
+	}
+	
+	@Test
+	public void isSQLInject() {
+		assertTrue(validate.isSQL("; DROP TABLE ASSERT_TABLE;"));
+	}
+	
+	@Test
+	public void returnPin() {
+		assertEquals(validate.parsePin("4444"), 4444);
 	}
 }
