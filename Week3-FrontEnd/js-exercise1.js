47ba6e1997c41cfe98aed612fc715f122b744604
omar-nav <omar.navarro@g.austincc.edu>
2019-06-13 23:01:14 -0500

    TODO 11 and 12

--- Week3-FrontEnd/js-exercise1.js
+++ Week3-FrontEnd/js-exercise1.js
@@ -200,5 +200,30 @@ function isPalindrome(someStr){
 // ([)]
   
 // Create a function which takes a string of brackets and returns true if balanced and false if not balanced
-  
-  
+function balanced(string){
+  let stack = [];
+  let map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+  }
+  for (let i = 0; i < string.length; i++) {
+      if (string[i] === '(' || string[i] === '{' || string[i] === '[' ) {
+          stack.push(string[i]);
+      }
+      else {
+          let last = stack.pop();
+          if (string[i] !== map[last]) {return false};
+      }
+  }
+      if (stack.length !== 0) {return false};
+  return true;
+}
+// balanced("({[]})");
+// balanced("(())");
+// balanced("()()");
+// balanced("()");
+// balanced("(");
+// balanced(")");
+// balanced("(()i");
+// balanced("([)]");
